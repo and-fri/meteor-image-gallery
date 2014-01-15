d89082c216bd8ff31ad1dfd07253de175b8ac3de
andrej fritz <a.fritz@betzold.de>
2014-01-15 11:40:17 +0100

    first init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meteor package: Blueimp image gallery
+Blueimp's image gallery packed for Meteor.
---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: "Blueimp's image gallery packed for Meteor"
+});
+
+Package.on_use(function(api) {
+  api.use(['jquery'], 'client');
+  api.add_files(
+    [
+      'lib/blueimp-gallery.js', 
+      'lib/blueimp-helper.js', 
+      'lib/blueimp-gallery.min.css', 
+      'lib/jquery.blueimp-gallery.js'
+    ]
+    , 'client'
+  );
+});
