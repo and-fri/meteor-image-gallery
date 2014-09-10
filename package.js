d6a56f8a6d5fba9c97a98212f1140177c9674dec
andrej fritz <a.fritz@betzold.de>
2014-09-10 16:20:35 +0200

    updated to meteor 0.9

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: "Blueimp's image gallery packed for Meteor"
+  summary: "Blueimp's image gallery packed for Meteor",
+  version: "0.0.1",
+  git: "https://github.com/andruschka/meteor-image-gallery.git"
 });
 
 Package.on_use(function(api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use(['jquery'], 'client');
   api.add_files(
     [
